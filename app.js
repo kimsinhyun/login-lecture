a670234c8e6a2ae2fa9205e31af416ccefb06147
kimsinhyun <ksh415622@naver.com>
2021-03-06 13:50:28 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";//이크마 스트립트 문법을 준수하겠다는 의미의 코드 -> 항상 작성해주는 것이 좋다
+
+const express = require('express');
+const app = express();
+
+
+//앱 셋팅 -> 뷰들을 셋팅하는 코드
+app.set("views", "./views");
+//뷰 엔진 셋팅하는 코드
+app.set("view engine", "ejs");
+
+const home = require("./routes/home")
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드
+
+module.exports = app;
